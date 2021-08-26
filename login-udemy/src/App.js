b56bffafcbad0986f30b8aea89856f5127a2dec3
SantiDani <profsantiRP@gmail.com>
2021-08-26 20:07:44 +0200

    21 - Use effect change class to function

--- login-udemy/src/App.js
+++ login-udemy/src/App.js
@@ -1,56 +1,17 @@
 import { useState, Component } from "react";
 
 
-class App extends Component{
+const App = () => {
 
-  // props is a good practique
-  constructor(props){
-    super(props);
-    console.log('I am the constructor');
+  const [name, setName] = useState("");
 
-    /* Is usefull declare constructor  if 
-    we will create variables of state */
-
-    this.state = {
-      name: "",
-    };
-
-  }
-
-  componentDidMount(){
-    console.log('I am the componentDidMount()');
+  return (
+    <div>
+      <h1>React component life cycle</h1>
+      <input value={name} onChange={({target: {value}}) => setName(value)}></input>
+    </div>
+  )
   
-  }
-
-  // snapshot value that return getSnapshotBeforeUpdate
-  componentDidUpdate(prevProps, prevState, snapshot){
-    
-    console.log('I am the componentDidUpdate()');
-  }
-
-  shouldComponentUpdate(){
-    
-    console.log('I am the shouldComponentUpdate()');
-    // return false not render
-    // return true render
-    // It is used for optimitze 
-    return true;
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState){
-    console.log('I am the getSnapshotBeforeUpdate()');
-    console.log('prevProps ', prevProps);
-    console.log('prevState ', prevState);
-  }
-
-  render(){
-    return (
-      <div>
-        <h1>React component life cycle</h1>
-        <input value={this.state.name} onChange={({target: {value}}) => this.setState({name: value})}></input>
-      </div>
-    )
-  }
 
 }
 
