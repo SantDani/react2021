a6eea93082d58b72f97bdb6f9e989bf381b82dcb
SantiDani <profsantiRP@gmail.com>
2021-08-26 21:17:04 +0200

    23 - List and Keys
    - useEffect
    - conditional rendering
    - jsonData

--- login-udemy/src/App.js
+++ login-udemy/src/App.js
@@ -1,50 +1,57 @@
 import { useState, useEffect } from "react";
 
 
+const jsonData = [{
+  key: "1",
+  name: "Armando",
+},
+{
+  key: "2",
+  name: "Vegeta",
+},
+{
+  key: "3",
+  name: "Ricardo",
+},
+{
+  key: "4",
+  name: "Segura",
+},
+{
+  key: "5",
+  name: "Notion",
+},
+{
+  key: "6",
+  name: "Web Strorm",
+}];
+
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-
-  // Component did Upadte
   useEffect(() => {
-    console.log(' useEffect - did Update');
-  });
+    setTimeout( () => {
+      setIsLoading(false);
+    }, 3000)
+  },[])
 
-  // componetDidMount
-  useEffect(() => {
-    // we can call some API
-    console.log('useEffect - componetDidMount');
+  const renderData = () => {
     
-    // WillUnmount
-    return () => {
-      console.log('useEffect - WillUnmount');
-    }
-  }, []);
+    // if(isLoading) return <h2> Is loading ...</h2>
+      
 
-  useEffect(() => {
-    console.log('useEffect - Name was updated');
-  }, [name]);
-
-  useEffect(() => {
-    console.log('useEffect - Age was updated');
-  }, [age]);
-
-  useEffect(() => {
-    console.log('useEffect - Name or Age was updated');
-  }, [name, age]);
-
-
+    return jsonData?.map((value) => (
+      <div key={value.key}>
+        <p>{value.name}</p>
+      </div>
+    ));
+      
+  };
   
   return (
     <div>
-      <h1>React component life cycle</h1>
-      <label>Name</label>
-      <input value={name} onChange={({target: {value}}) => setName(value)}></input>
-      <br></br>
-      <br></br>
-      <label>Age</label>
-      <input value={age} onChange={({target: {value}}) => setAge(value)}></input>
+      <h1>List and keys</h1>
+      { isLoading ? <h2> Is loading ...</h2> :  renderData()}
     </div>
   )
   
