be4960a000f696e6e05e42c5923b344b4c9d5315
SantiDani <profsantiRP@gmail.com>
2021-08-27 12:43:44 +0200

    Router
    - Switch Router
    - Route
    - path
     exact

--- router/src/App.js
+++ router/src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Routes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Routing</h1>
-      </header>
-    </div>
+    <Routes/>
   );
 }
 
