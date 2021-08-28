25010304ef1c400f27625d03833bfcbdfc66ff2a
SantiDani <profsantiRP@gmail.com>
2021-08-28 20:36:40 +0200

    Scroll to top

--- router/src/routes/index.js
+++ router/src/routes/index.js
@@ -9,15 +9,18 @@ import {
 import Home from "./../views/home"
 import PageNotFound from "../views/404";
 import PokeDetail from "../views/PokeDetail";
+import useScrollToTop from "../components/ScrollToTop";
+import ScrollToTop from "../components/ScrollToTop";
 
 
 export default function Routes(){
-
+    
     return(
         <Router>
             {/* exact It is only works with the exact route.
                 <Route path="/" exact>  = only and only if "/"
             */}
+            <ScrollToTop/>
             <Switch>
                 <Route path="/" exact> 
                     <Home/>
