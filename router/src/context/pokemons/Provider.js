00cd3b068d3077787f8a6cdbe6accca21c1c2c02
SantiDani <profsantiRP@gmail.com>
2021-08-28 20:04:55 +0200

    call detail pokemon by Provider

--- router/src/context/pokemons/Provider.js
+++ router/src/context/pokemons/Provider.js
@@ -4,14 +4,15 @@ import PokemonContext from ".";
 import apiCall from "../../api";
 export default function PokemonProvider({children}) {
     // const URL_API = 'https://pokeapi.co/api/v2/pokemon/ditto';
-    const URL_API = 'https://pokeapi.co/api/v2/pokemon?limit=100&offset=0';
+    const URL_API = 'https://pokeapi.co/api/v2/';
 
     const [pokemons, setPokemons] = useState([]);
+    const [pokemonDetail, setPokemonDetail] = useState({});
 
     const getPokemons = async () => {
 
         try {
-            const pokemonResult = await apiCall({url: `${URL_API}`});
+            const pokemonResult = await apiCall({url: `${URL_API}pokemon?limit=100&offset=0`});
             // console.log('result apiCall',  pokemonResult);
             setPokemons(pokemonResult.results);
         } catch (e) {
@@ -20,8 +21,30 @@ export default function PokemonProvider({children}) {
         }
 
     }
+
+    const getPokemonDetail = async (id) =>{
+
+        if (!id) Promise.reject("ID is necessary");
+
+        try {
+            console.log('log - id to call API ', id);
+            const pokemonResult = await apiCall({url: `${URL_API}pokemon/${id}`});
+            console.log('log pokemonResult', pokemonResult);
+            setPokemonDetail(pokemonResult);
+            
+        } catch (e) {
+            setPokemonDetail({});
+            console.error(e);
+            
+        }
+    }
+
     return (
-        <PokemonContext.Provider value={{ getPokemons, pokemons}}>
+        <PokemonContext.Provider 
+            value={{ 
+                getPokemons, pokemons,
+                getPokemonDetail, pokemonDetail
+                }}>
             {children}
         </PokemonContext.Provider>
     );
--- router/src/views/PokeDetail/index.js
+++ router/src/views/PokeDetail/index.js
@@ -1,20 +1,25 @@
-import React from "react";
-// import ReactDOM from "react-dom";
-import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-  useParams
-} from "react-router-dom";
+import React, { useEffect, useContext} from "react";
+ import {useParams} from "react-router-dom";
+ import PokemonContext from "../../context/pokemons";
 
 
 export default function PokeDetail(){
     let {id} = useParams();
+    const {getPokemonDetail, pokemonDetail} = useContext(PokemonContext);
 
+    /**Every time that load page 
+     *  - Get detail pokemon from his ID
+     */
+
+    useEffect(() => {
+        getPokemonDetail(id).catch(e => console.error(e));
+
+        // console.log('log - in Poke detail', pokemonDetail);
+    }, [])
     return (
         <div>
-            
             <h2>Poke Detail {id}</h2>
+            {pokemonDetail.name}
         </div>
     )
 }
